70383b33befc66b95a07f9a9153dc7de0003cb0c
Dmitry Titov <ditriy.titov@mail.ru>
2024-02-14 11:32:50 +0500

    Initial commit

---
+++ app/src/main/java/ru/netology/nework/auxiliary/FloatingValue.kt
@@ -0,0 +1,23 @@
+package ru.netology.nework.auxiliary
+
+import android.content.ContentResolver
+import android.net.Uri
+import android.webkit.MimeTypeMap
+
+
+object FloatingValue {
+    var textNewPost = ""
+    var currentFragment = ""
+
+    fun convertDatePublished(dateString: String): String {
+        val date = dateString.substring(0..9)
+        val time = dateString.substring(11..18)
+        return "$date $time"
+    }
+
+    fun getExtensionFromUri(uri: Uri, contentResolver: ContentResolver): String? {
+        val mimeType = contentResolver.getType(uri) ?: return null
+        return MimeTypeMap.getSingleton().getExtensionFromMimeType(mimeType)
+    }
+
+}
